766a1b749a0ec5b35b57896dd479546c5f211f24
parmeshwar <parmeshwar92@gmail.com>
2022-11-12 15:14:39 +0530

    added logger

--- src/main/java/com/tcs/account/controller/AccountController.java
+++ src/main/java/com/tcs/account/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.tcs.account.controller;
 import com.tcs.account.entity.Accounts;
 import com.tcs.account.modal.CustomerRequest;
 import com.tcs.account.repository.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
@@ -13,11 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountController {
 
+    Logger logger= LoggerFactory.getLogger(AccountController.class);
+
     @Autowired
     private AccountRepository accountRepository;
 
     @PostMapping("/tcs/account-info")
     ResponseEntity<Accounts> getAccount(@RequestBody CustomerRequest customerRequest){
+        logger.info("request for customer account info");
         Accounts accounts = accountRepository.findByCustomerId(customerRequest.getCustomerId());
         if (accounts != null) {
             return ResponseEntity.ok(accounts);
